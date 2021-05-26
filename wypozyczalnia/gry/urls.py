d16d31682dc5e10a3e129d65662416856df7916a
Dadits <goldenbiq@gmail.com>
2021-05-26 13:19:00 +0200

    Pierwszy widok

--- wypozyczalnia/gry/urls.py
+++ wypozyczalnia/gry/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'wypozyczalnia'
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
