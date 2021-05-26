e3700b80fbd3ec4a05b113109eda18ed1362bd98
Dadits <goldenbiq@gmail.com>
2021-05-26 13:11:27 +0200

    Zmiana urls

--- wypozyczalnia/gry/urls.py
+++ wypozyczalnia/gry/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
