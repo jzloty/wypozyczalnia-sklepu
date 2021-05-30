5cdf294c1958e4565ff45af3b99bce7cb5f45947
Dadits <goldenbiq@gmail.com>
2021-05-30 11:50:10 +0200

    Zmiana wyswietlania listy przedmiotow w panelu admina

--- wypozyczalnia/gry/admin.py
+++ wypozyczalnia/gry/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import NazwaGry
 
-admin.site.register(NazwaGry)
+
+class NazwaGryAdmin(admin.ModelAdmin):
+    list_display = ('Tytul', 'Data_orientacyjny_zwrot',
+                    'Dostepnosc', 'Data_wypozyczenie')
+
+
+admin.site.register(NazwaGry, NazwaGryAdmin)
