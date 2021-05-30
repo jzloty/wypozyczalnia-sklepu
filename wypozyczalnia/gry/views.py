da8045875bde4504116f48ec6d3190be501db13e
Dadits <goldenbiq@gmail.com>
2021-05-30 11:55:52 +0200

    Sortowanie glownej listy gier

--- wypozyczalnia/gry/views.py
+++ wypozyczalnia/gry/views.py
@@ -3,6 +3,6 @@ from gry.models import NazwaGry
 
 
 def index(request):
-    Lista_gier = NazwaGry.objects.all()
+    Lista_gier = NazwaGry.objects.all().order_by('Tytul')
     context = {'Lista_gier': Lista_gier}
     return render(request, 'gry/index.html', context)
