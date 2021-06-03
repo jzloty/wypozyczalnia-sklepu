16c339861bc0852f46996552ef968641b5dfe9be
Dadits <goldenbiq@gmail.com>
2021-06-03 15:46:16 +0200

    Nowy widok AJAX

--- wypozyczalnia/gry/views.py
+++ wypozyczalnia/gry/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from gry.models import NazwaGry
+import json
+from django.http import JsonResponse
 
 
 def index(request):
     Lista_gier = NazwaGry.objects.all().order_by('Tytul')
     context = {'Lista_gier': Lista_gier}
     return render(request, 'gry/index.html', context)
+
+
+def szukaj(request):
+    if request.method == 'POST':
+        szukane = json.loads(request.body).get('szukanyText')
+
+        wynik = NazwaGry.objects.filter(Tytul__icontains=szukane) | \
+            NazwaGry.objects.filter(
+                Data_orientacyjny_zwrot__stars_with=szukane)
+        dane = wynik.values()
+
+        return JsonResponse(list(dane), safe=False)
